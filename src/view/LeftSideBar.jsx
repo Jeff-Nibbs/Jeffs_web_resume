be78a0d1714b7e19ed95093d3a917cbf2da5c1dd
Jeffrey Nibbs <danielnibbs100@gmail.com>
2026-02-07 23:20:34 -0800

    speed up opening animations

--- src/view/LeftSideBar.jsx
+++ src/view/LeftSideBar.jsx
@@ -1,59 +1,112 @@
-import { useRef, useEffect } from 'react'
-import { FiLinkedin } from 'react-icons/fi'
-import { FiGithub } from 'react-icons/fi'
-import { FiMail } from 'react-icons/fi'
-import { FiFileText } from 'react-icons/fi'
-import cv from '../assets/Daniel-cv.pdf'
+import { useRef, useEffect } from "react";
+import { FiLinkedin } from "react-icons/fi";
+import { FiGithub } from "react-icons/fi";
+import { FiMail } from "react-icons/fi";
+import { FiFileText } from "react-icons/fi";
+import cv from "../assets/Daniel-cv.pdf";
 // import gsap
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 function LeftSideBar() {
   // register ScrollTrigger
-  gsap.registerPlugin(ScrollTrigger)
+  gsap.registerPlugin(ScrollTrigger);
   // set up ref for leftSidebar
-  const leftSidebar = useRef(null)
+  const leftSidebar = useRef(null);
 
   // build gsap timeline
   useEffect(() => {
     let ctx = gsap.context(() => {
       gsap
         .timeline()
-        .to('.side-line', { height: 112, duration: 3, ease: 'power4.inOut', delay: 2.5 })
-        .from('.side-link1', { autoAlpha: 0, y: -50, duration: 0.8, ease: 'power4.out', delay: 0.1 }, '-=1')
-        .from('.side-link2', { autoAlpha: 0, y: -50, duration: 0.8, ease: 'power4.out', delay: 0.1 }, '-=0.85')
-        .from('.side-link3', { autoAlpha: 0, y: -50, duration: 0.8, ease: 'power4.out', delay: 0.1 }, '-=0.7')
-        .from('.side-link4', { autoAlpha: 0, y: -50, duration: 0.8, ease: 'power4.out', delay: 0.1 }, '-=0.7')
+        .to(".side-line", {
+          height: 112,
+          duration: 1.5,
+          ease: "back.out(1.7)",
+          delay: 1,
+        })
+        .from(
+          ".side-link1",
+          {
+            autoAlpha: 0,
+            y: -50,
+            duration: 0.5,
+            ease: "back.out(1.7)",
+            delay: 0.1,
+          },
+          "-=1",
+        )
+        .from(
+          ".side-link2",
+          {
+            autoAlpha: 0,
+            y: -50,
+            duration: 0.5,
+            ease: "back.out(1.7)",
+            delay: 0.1,
+          },
+          "-=0.85",
+        )
+        .from(
+          ".side-link3",
+          {
+            autoAlpha: 0,
+            y: -50,
+            duration: 0.5,
+            ease: "back.out(1.7)",
+            delay: 0.1,
+          },
+          "-=0.7",
+        )
+        .from(
+          ".side-link4",
+          {
+            autoAlpha: 0,
+            y: -50,
+            duration: 0.5,
+            ease: "back.out(1.7)",
+            delay: 0.1,
+          },
+          "-=0.7",
+        )
         .to(leftSidebar.current, {
           y: 500,
           scrollTrigger: {
             trigger: leftSidebar.current,
-            start: 'top top',
-            end: '+=800',
+            start: "top top",
+            end: "+=800",
             scrub: 0.2,
           },
-        })
-    }, leftSidebar)
-    return () => ctx.revert()
-  }, [])
+        });
+    }, leftSidebar);
+    return () => ctx.revert();
+  }, []);
 
   return (
-    <div ref={leftSidebar} className='flex flex-col justify-center items-center gap-3 lg:gap-5 fixed bottom-2 left-5 lg:left-14 z-10'>
-      <a href='https://github.com/Jeff-Nibbs' target='_blank' rel='noopener noreferrer'>
-        <FiGithub className='translate-y-0 side-link1 invisible text-gray-600 text-3xl lg:text-4xl cursor-pointer hover:text-sky-400' />
+    <div
+      ref={leftSidebar}
+      className="flex flex-col justify-center items-center gap-3 lg:gap-5 fixed bottom-2 left-5 lg:left-14 z-10">
+      <a
+        href="https://github.com/Jeff-Nibbs"
+        target="_blank"
+        rel="noopener noreferrer">
+        <FiGithub className="translate-y-0 side-link1 invisible text-gray-600 text-3xl lg:text-4xl cursor-pointer hover:text-sky-400" />
       </a>
-      <a href='https://www.linkedin.com/in/daniel-nibbs/' target='_blank' rel='noopener noreferrer'>
-        <FiLinkedin className='translate-y-0 side-link2 invisible text-gray-600 text-3xl lg:text-4xl cursor-pointer hover:text-sky-400' />
+      <a
+        href="https://www.linkedin.com/in/daniel-nibbs/"
+        target="_blank"
+        rel="noopener noreferrer">
+        <FiLinkedin className="translate-y-0 side-link2 invisible text-gray-600 text-3xl lg:text-4xl cursor-pointer hover:text-sky-400" />
       </a>
-      <a href='mailto:danielnibbs100@gmail.com'>
-        <FiMail className='translate-y-0 side-link3 invisible text-gray-600 text-3xl lg:text-4xl cursor-pointer hover:text-sky-400' />
+      <a href="mailto:danielnibbs100@gmail.com">
+        <FiMail className="translate-y-0 side-link3 invisible text-gray-600 text-3xl lg:text-4xl cursor-pointer hover:text-sky-400" />
       </a>
       <a href={cv} download>
-        <FiFileText className='translate-y-0 side-link4 invisible text-gray-600 text-3xl lg:text-4xl cursor-pointer hover:text-sky-400' />
+        <FiFileText className="translate-y-0 side-link4 invisible text-gray-600 text-3xl lg:text-4xl cursor-pointer hover:text-sky-400" />
       </a>
-      <div className='side-line m-0 w-0.5 h-0 bg-gray-600'></div>
+      <div className="side-line m-0 w-0.5 h-0 bg-gray-600"></div>
     </div>
-  )
+  );
 }
 
-export default LeftSideBar
+export default LeftSideBar;
--- src/view/Nav.jsx
+++ src/view/Nav.jsx
@@ -1,80 +1,106 @@
-import { useEffect, useRef } from 'react'
-import { gsap } from 'gsap'
-import { Link, animateScroll as scroll } from 'react-scroll'
-import cv from '../assets/Daniel-cv.pdf'
-import logo from '../assets/d-button-icon.svg'
-import '../view/Nav.css'
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { Link, animateScroll as scroll } from "react-scroll";
+import cv from "../assets/Daniel-cv.pdf";
+import logo from "../assets/d-button-icon.svg";
+import "../view/Nav.css";
 function Nav() {
   // set up ref for nav
-  const nav = useRef(null)
+  const nav = useRef(null);
 
   // build gsap timeline
   useEffect(() => {
     let ctx = gsap.context(() => {
       gsap
         .timeline()
-        .from('.logo', { autoAlpha: 0, y: -50, duration: 1, ease: 'power4.out', delay: 3.7 })
-        .from('.nav1', { autoAlpha: 0, y: -50, duration: 1, ease: 'power4.out' }, '-=0.75')
-        .from('.nav2', { autoAlpha: 0, y: -50, duration: 1, ease: 'power4.out' }, '-=0.75')
-        .from('.nav3', { autoAlpha: 0, y: -50, duration: 1, ease: 'power4.out' }, '-=0.75')
-        .from('.nav4', { autoAlpha: 0, y: -50, duration: 1, ease: 'power4.out' }, '-=0.75')
-    }, nav)
-    return () => ctx.revert()
-  }, [])
+        .from(".logo", {
+          autoAlpha: 0,
+          y: -50,
+          duration: 0.5,
+          ease: "power4.out",
+          delay: 1,
+        })
+        .from(
+          ".nav1",
+          { autoAlpha: 0, y: -50, duration: 1, ease: "power4.out" },
+          "-=0.7",
+        )
+        .from(
+          ".nav2",
+          { autoAlpha: 0, y: -50, duration: 1, ease: "power4.out" },
+          "-=0.7",
+        )
+        .from(
+          ".nav3",
+          { autoAlpha: 0, y: -50, duration: 1, ease: "power4.out" },
+          "-=0.7",
+        )
+        .from(
+          ".nav4",
+          { autoAlpha: 0, y: -50, duration: 1, ease: "power4.out" },
+          "-=0.7",
+        );
+    }, nav);
+    return () => ctx.revert();
+  }, []);
 
   return (
-    <nav ref={nav} className='flex p-auto justify-center items-center fixed min-w-full py-3 md:px-4 z-10'>
-      <Link to='hero' smooth={true} delay={150} duration={1000} isDynamic={true} className='mr-auto cursor-pointer ml-7 hidden md:block '>
-        <img src={logo} alt='logo' className='logo w-5 md:w-16 ' />
+    <nav
+      ref={nav}
+      className="flex p-auto justify-center items-center fixed min-w-full py-3 md:px-4 z-10">
+      <Link
+        to="hero"
+        smooth={true}
+        delay={150}
+        duration={1000}
+        isDynamic={true}
+        className="mr-auto cursor-pointer ml-7 hidden md:block ">
+        <img src={logo} alt="logo" className="logo w-5 md:w-16 " />
       </Link>
-      <div className='flex justify-center items-center gap-10 md:gap-7 md:mr-7 font-lato font-light'>
+      <div className="flex justify-center items-center gap-10 md:gap-7 md:mr-7 font-lato font-light">
         <Link
-          to='about'
+          to="about"
           smooth={true}
           delay={150}
           duration={1000}
           isDynamic={true}
           offset={100}
-          className='invisible link nav1 text-sm md:text-lg cursor-pointer text-gray-600'
-        >
+          className="invisible link nav1 text-sm md:text-lg cursor-pointer text-gray-600">
           ABOUT
         </Link>
 
         <Link
-          to='projects'
+          to="projects"
           smooth={true}
           delay={150}
           duration={1000}
           isDynamic={true}
           offset={50}
-          className='invisible link nav2 text-sm md:text-lg cursor-pointer text-gray-600'
-        >
+          className="invisible link nav2 text-sm md:text-lg cursor-pointer text-gray-600">
           PROJECTS
         </Link>
 
         <Link
-          to='contact'
+          to="contact"
           smooth={true}
           delay={150}
           duration={1000}
           isDynamic={true}
           offset={100}
-          className='invisible link nav3 text-sm md:text-lg cursor-pointer text-gray-600'
-        >
+          className="invisible link nav3 text-sm md:text-lg cursor-pointer text-gray-600">
           CONTACT
         </Link>
 
         <a
           href={cv}
-          target='_blank'
-          rel='noopener noreferrer'
-          className='invisible nav4 flex justify-center item-center border-2 rounded py-1 px-2 text-sm md:py-3 md:px-4 cursor-pointer text-sky-400 border-sky-500 hover:text-gray-50 hover:bg-sky-400'
-        >
+          target="_blank"
+          rel="noopener noreferrer"
+          className="invisible nav4 flex justify-center item-center border-2 rounded py-1 px-2 text-sm md:py-3 md:px-4 cursor-pointer text-sky-400 border-sky-500 hover:text-gray-50 hover:bg-sky-400">
           RESUME
         </a>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
